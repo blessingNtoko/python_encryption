d3618c1fb3d9e94bac19811e1c1e1f26286cb51c
Blessing Ntoko <bntoko@gmail.com>
2019-12-07 09:13:33 +0200

    added key gen file || changed how key is generated in file encryption method

--- fileEncryptDecrypt.py
+++ fileEncryptDecrypt.py
@@ -1,8 +1,27 @@
 from cryptography.fernet import Fernet
+import base64
+import os
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+
+
+# Generate key from password
+password_provided = input("Please type in your password in order to generate a key: ") # This is input in the form of a string
+password = password_provided.encode() # Convert to type bytes
+salt = os.urandom(16)
+kdf = PBKDF2HMAC(
+    algorithm=hashes.SHA256(),
+    length=32,
+    salt=salt,
+    iterations=100000,
+    backend=default_backend()
+)
+key = base64.urlsafe_b64encode(kdf.derive(password))
+print(key)
 
 
 # Encrypt
-key = Fernet.generate_key()
 input_file = 'test.txt'
 output_file = 'test.encrypted'
 
---
+++ genKeyFromPass.py
@@ -0,0 +1,18 @@
+import base64
+import os
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+
+password_provided = input("Please type in your password in order to generate a key: ") # This is input in the form of a string
+password = password_provided.encode() # Convert to type bytes
+salt = os.urandom(16)
+kdf = PBKDF2HMAC(
+    algorithm=hashes.SHA256(),
+    length=32,
+    salt=salt,
+    iterations=100000,
+    backend=default_backend()
+)
+key = base64.urlsafe_b64encode(kdf.derive(password)) # Can only use kdf once
+print(key)
